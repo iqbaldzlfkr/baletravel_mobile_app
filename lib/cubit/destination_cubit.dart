d1f13992a792875866e7aa8fef44f7ca8d66e404
Iqbal-Dzulfikar <iqbald2015@gmail.com>
2023-08-19 17:37:15 +0700

    destination state dan destination model

---
+++ lib/cubit/destination_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bale_travel/models/destination_model.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'destination_state.dart';
+
+class DestinationCubit extends Cubit<DestinationState> {
+  DestinationCubit() : super(DestinationInitial());
+}
